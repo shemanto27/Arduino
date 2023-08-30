56e983edca58d90acbbaaeb8385861fc6f58dcdd
Bidut Sharkar Shemanto <84231263+shemanto27@users.noreply.github.com>
2023-08-31 00:31:08 +0600

    Add files via upload

---
+++ basic_practices/driving_multiple_leds_with_arduino1.ino
@@ -0,0 +1,50 @@
+
+
+
+void setup()
+{
+pinMode(6, OUTPUT);
+pinMode(7, OUTPUT);
+pinMode(8, OUTPUT);
+pinMode(9, OUTPUT);
+pinMode(10, OUTPUT);
+pinMode(11, OUTPUT);
+pinMode(12, OUTPUT);
+pinMode(13, OUTPUT);
+
+}
+
+void loop()
+{
+  digitalWrite(6, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  
+digitalWrite(7, HIGH);
+  delay(500);
+
+digitalWrite(8, HIGH);
+  delay(500);
+
+
+
+digitalWrite(9, HIGH);
+  delay(500);
+
+
+
+digitalWrite(10, HIGH);
+  delay(500);
+
+
+
+digitalWrite(11, HIGH);
+  delay(500);
+
+digitalWrite(12, HIGH);
+  delay(500);
+
+digitalWrite(13, HIGH);
+  delay(500);
+
+  
+}
---
+++ basic_practices/simple_led_with_arduino_project_o_1.ino
@@ -0,0 +1,10 @@
+void setup()
+{
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  }
